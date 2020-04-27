96eee1d12bde87dbeee7e0e3c6d459e5fe1e5326
PhucPNT <pn.truongphuc@gmail.com>
2020-04-27 22:42:35 +0700

    feat: invest as a new default tab

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -1,6 +1,7 @@
 import { WEBUI_BASE_URL, WEBUI_URL_SUFFIX } from './files';
 
-export const NEWTAB_URL = `${WEBUI_BASE_URL}newtab${WEBUI_URL_SUFFIX}`;
+// export const NEWTAB_URL = `${WEBUI_BASE_URL}newtab${WEBUI_URL_SUFFIX}`;
+export const NEWTAB_URL = `${WEBUI_BASE_URL}_invest${WEBUI_URL_SUFFIX}`;
 
 export const defaultTabOptions: chrome.tabs.CreateProperties = {
   url: NEWTAB_URL,
